15c8781606ae5dda45138b62c526bf04d4a50ba0
Serge Kovbasiuk <isserge@protonmail.com>
2024-08-09 11:23:22 +0700

    feat: Update Makefile and config to use deployed addresses and environment variables

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,14 +1,26 @@
 import dotenv from 'dotenv';
 import { z } from 'zod';
 
+import addresses from './deployed_addresses.json';
+
 dotenv.config();
 
-const ConfigSchema = z.object({
-  PRIVATE_KEY: z.string().regex(/^0x[a-fA-F0-9]{64}$/, "Invalid private key"),
+const AddressesSchema = z.object({
   DRAND_ORACLE_ADDRESS: z.string().regex(/^0x[a-fA-F0-9]{40}$/, "Invalid Ethereum address"),
   SEQUENCER_ORACLE_ADDRESS: z.string().regex(/^0x[a-fA-F0-9]{40}$/, "Invalid Ethereum address"),
   RANDOMNESS_ORACLE_ADDRESS: z.string().regex(/^0x[a-fA-F0-9]{40}$/, "Invalid Ethereum address"),
+});
+
+const addressesConfig = AddressesSchema.parse(addresses);
+
+const EnvConfigSchema = z.object({
+  PRIVATE_KEY: z.string().regex(/^0x[a-fA-F0-9]{64}$/, "Invalid private key"),
   DRAND_HTTP_URL: z.string().url(),
 });
 
-export const config = ConfigSchema.parse(process.env);
+const envConfig = EnvConfigSchema.parse(process.env);
+
+export const config = {
+  ...addressesConfig,
+  ...envConfig,
+};
